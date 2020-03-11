1ca8584d313bb0b761ef9c2ef5bb125d062ef490
Noah Emmenegger <noahemmenegger.business@gmail.com>
2020-03-11 10:25:46 +0100

    add change color

--- Scene/GameScene.js
+++ Scene/GameScene.js
@@ -6,14 +6,16 @@ class GameScene extends Phaser.Scene{
 
     preload() {
         this.load.audio('music', ['assets/Music.mp3']);
-        this.load.image('ball_red', './assets/ball.png');
+        this.load.image('ball_blue', './assets/ball_blue.png');
+        this.load.image('ball_green', './assets/ball_green.png');
+        this.load.image('ball_red', './assets/ball_red.png');
         this.load.image('bar_blue', './assets/bar_blue.png');
         this.load.image('bar_green', './assets/bar_green.png');
         this.load.image('bar_red', './assets/bar_red.png');
 
         this.distance = 1000; //in ms
         this.speed = 5;
-        this.activeBall = 'ball_red'
+        this.activeBall = this.getRandomBallColor();
     }
 
     create() {
@@ -23,7 +25,7 @@ class GameScene extends Phaser.Scene{
         this.leftkeyDown = false;
         this.rightKeyDown = false;
         this.player = this.physics.add.sprite(300, 600, this.activeBall);
-        this.player.setScale(0.1);
+        this.player.setScale(0.05);
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.score = 0;
@@ -101,6 +103,19 @@ class GameScene extends Phaser.Scene{
         var bar_red = this.physics.add.sprite(randomarray[2], 0, 'bar_red');
         var physic = this.physics.add.overlap([bar_red, bar_green, bar_blue], this.player, (bar, ball) => this.checkCollisionColor(bar, ball, physic), null, this);
         this.containers.push(container.add([bar_blue, bar_green, bar_red]));
+        var containerPhysic = this.physics.add.overlap([bar_red, bar_green, bar_blue], this.player, (bar, ball) => this.changeColor(bar, ball, container), null, this);
+    }
+
+    changeColor(bar, ball, container) {
+        if(container.y == 600) {
+            this.activeBall = this.getRandomBallColor();
+            this.player.setTexture(this.activeBall);
+        }
+    }
+
+    getRandomBallColor() {
+        var colors = ['ball_red', 'ball_green', 'ball_blue']
+        return colors[Phaser.Math.Between(0,2)]
     }
     
     checkCollisionColor(bar, ball, physic) {
