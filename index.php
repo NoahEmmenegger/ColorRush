7d317c1931d8edaaac5df5d9b29868fc8ed1e755
Nicolas Bürgler <trajan15.nb@gmail.com>
2020-03-18 09:56:10 +0100

    Hinzugefüt dass "localhost/ColorRush" auf Login verweisst

--- index.php
+++ index.php
@@ -5,16 +5,20 @@
   $url = $_GET['url'] ?? "";
   $url = rtrim($url, '/');
   $url = explode('/', $url);
+  if($url[0] == '')
+  {
+    $url[0] = 'Login';
+  }
   //Das File, welches der Url entspricht
   $file = 'Controller/' . $url[0] . '.php';
-  if(file_exists($file))
+    if(file_exists($file))
   {
     include $file;  
   }
   else{
     echo "Diese Url exisitert nicht. Fehler: 404";
   }
-
+  
   ?>
     <body>
 
